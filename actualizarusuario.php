193e77b0693d0c70ad760b664ef6b31d4f34e34a
John Esteban Astete Arambarri <john.ea@hotmail.com>
2020-02-18 19:39:58 -0300

    Update actualizarusuario.php
    Ahora se puede editar usuarios, pendiente la contraseña cuando sea distinta de vacío

--- actualizarusuario.php
+++ actualizarusuario.php
@@ -1,55 +1,51 @@
 <?php session_start();
-	if(!isset($_SESSION['usuario'])){
+if(!isset($_SESSION['usuario'])){
 	header('Location: login.php');
-	}
-	require 'funciones.php';
-	try{
-		$conexion = new PDO('mysql:host=localhost;dbname=centromedico','root','');
-	}catch(PDOException $e){
-		echo "ERROR: " . $e->getMessge();
-		die();
-	}
-	
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		$id = limpiarDatos($_POST['id']);
-		$identificacion = limpiarDatos($_POST['identificacion']);
-		$nombres = limpiarDatos($_POST['nombres']);
-		$apellidos = limpiarDatos($_POST['apellidos']);
-		$correo = limpiarDatos($_POST['correo']);
-		$telefono = limpiarDatos($_POST['telefono']);
-		$especialidad = limpiarDatos($_POST['especialidad']);
-		
-		$statement = $conexion->prepare(
-		"UPDATE medicos
-        SET medidentificacion = :identificacion, 
-		mednombres =:nombres, 
-		medapellidos =:apellidos, 
-		medEspecialidad =:especialidad, 
-		medtelefono =:telefono, 
-		medcorreo =:correo 
-		WHERE idMedico = :id");
+}
+require 'funciones.php';
+try{
+	$conexion = new PDO('mysql:host=localhost;dbname=centromedico','root','');
+}catch(PDOException $e){
+	echo "ERROR: " . $e->getMessge();
+	die();
+}
 
-		$statement ->execute(array(
-        ':identificacion'=>$identificacion, 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	$id = limpiarDatos($_POST['id']);
+	$usuario = limpiarDatos($_POST['usuario']);
+	$password = limpiarDatos($_POST['password']);
+	$password2 = limpiarDatos($_POST['password2']);
+	$nombres = limpiarDatos($_POST['nombres']);
+	$apellidos = limpiarDatos($_POST['apellidos']);
+	$roll = limpiarDatos($_POST['roll']);
+	
+	$statement = $conexion->prepare(
+		"UPDATE usuarios
+		SET nombres =:nombres, 
+		apellidos =:apellidos, 
+		usuario =:usuario, 
+		roll =:roll
+		WHERE id = :id");
+
+	$statement ->execute(array(
 		':nombres'=>$nombres, 
 		':apellidos'=>$apellidos, 
-		':especialidad'=>$especialidad, 
-		':telefono'=>$telefono, 
-		':correo'=>$correo,
-        ':id'=> $id
-        ));
-        header('Location: usuarios.php');
-	}else{
-		$id_usuario = id_numeros($_GET['id']);
-		if(empty($id_usuario)){
-			header('Location: usuarios.php');
-		}
-		$user = obtenerUser_id($conexion,$id_usuario);
-		
-		if(!$user){
-			header('Location: usuarios.php');
-		}
-		$user =$user[0];
+		':usuario'=>$usuario, 
+		':roll'=>$roll,
+		':id'=> $id
+	));
+	header('Location: usuarios.php');
+}else{
+	$id_usuario = id_numeros($_GET['id']);
+	if(empty($id_usuario)){
+		header('Location: usuarios.php');
 	}
-	require 'vista/actualizarusuario.php';
-?>
+	$user = obtenerUser_id($conexion,$id_usuario);
+	
+	if(!$user){
+		header('Location: usuarios.php');
+	}
+	$user =$user[0];
+}
+
+require 'vista/actualizarusuario.php';
